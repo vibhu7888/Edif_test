b6b55d2aaa22bc6f7ebd8d013314e086b02b1d80
Vibhu Sharma <vibhu.sharma@edifecs.com>
2024-12-10 22:41:04 +0530

    2nd commit

--- remv1.java
+++
@@ -1,9 +0,0 @@
-public class remv1{
-    public static void main(String[] args) {
-        System.out.println("Quantum computing is poised to revolutionize the tech landscape by solving complex problems that are currently \r\n" + //
-                        "beyond the reach of classical computers. Leveraging the principles of quantum mechanics, these computers can \r\n" + //
-                        "process vast amounts of data simultaneously, making them ideal for applications in cryptography, material \r\n" + //
-                        "science, and drug discovery. As research and development in this field progress, we can expect significant \r\n" + //
-                        "breakthroughs that will transform industries and drive innovation.");
-    }
-}
