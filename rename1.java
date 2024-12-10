c3927f77c14107fd050312f1c1b7684aa154ac8c
Vibhu Sharma <vibhu.sharma@edifecs.com>
2024-12-10 22:03:04 +0530

    First Commit

---
+++ rename1.java
@@ -0,0 +1,12 @@
+public class rename1{
+    public static void main(String[] args) {
+        System.out.println("Edge computing is revolutionizing the way data is processed and analyzed by bringing \r\n" + //
+                        "computation closer to the data source. This approach reduces latency and bandwidth \r\n" + //
+                        "usage, making it ideal for applications that require real-time processing, such as \r\n" + //
+                        "autonomous vehicles, smart cities, and IoT devices. By processing data locally at the \r\n" + //
+                        "“edge” of the network, edge computing enhances performance, improves security, and \r\n" + //
+                        "enables faster decision-making. As the number of connected devices continues to grow, \r\n" + //
+                        "edge computing is becoming increasingly vital for managing and utilizing the vast \r\n" + //
+                        "amounts of data generated.");
+    }
+}
---
+++ skip_stg1.java
@@ -0,0 +1,9 @@
+public class skip_stg1{
+    public static void main(String[] args) {
+        System.out.println("Blockchain Technology and Its Impact: Blockchain technology is transforming various industries by providing a \r\n" + //
+                        "secure and transparent way to record transactions. Originally developed for cryptocurrencies like Bitcoin, \r\n" + //
+                        "blockchain’s decentralized ledger system ensures that data is immutable and tamper-proof. This technology is \r\n" + //
+                        "now being applied beyond finance, including supply chain management, where it enhances  traceability and \r\n" + //
+                        "reduces fraud, and in healthcare, where it secures patient records and ensures data integrity.");
+    }
+}
